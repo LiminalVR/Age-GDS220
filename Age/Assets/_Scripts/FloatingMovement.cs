375881adff28a06334a406e5730e11eac5284bff
Geurtsy <36181392+Geurtsy@users.noreply.github.com>
2018-06-19 18:31:34 +0800

    Movement Functionality Added

---
+++ Age/Assets/_Scripts/FloatingMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FloatingMovement : MonoBehaviour {
+
+    [SerializeField] private float _height;
+    [SerializeField] private float _maxSpeed;
+    [SerializeField] private float _accelaration;
+    private Rigidbody _rb;
+    private Vector3 _startPos;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _startPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if(transform.position.y > _height + _startPos.y && _accelaration > 0)
+            _accelaration *= -1;
+        else if(transform.position.y < -_height + _startPos.y && _accelaration < 0)
+            _accelaration *= -1;
+
+        _rb.AddForce(transform.up * _accelaration * Time.fixedDeltaTime);
+
+        _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _maxSpeed);
+    }
+}
