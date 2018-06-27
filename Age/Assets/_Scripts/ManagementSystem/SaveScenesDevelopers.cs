76375382c256a43830d3933ca733520e6d45abc6
Pommanthebrit <warburmf@hotmail.com>
2018-06-27 15:45:36 +0800

    Added a prompt to save if we changed the scene

---
+++ Age/Assets/_Scripts/ManagementSystem/SaveScenesDevelopers.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public class SaveScenesDevelopers {
+
+    static SaveScenesDevelopers()  {
+
+        EditorApplication.playModeStateChanged += LoadDefultScene;
+       
+}
+
+    static void LoadDefultScene(PlayModeStateChange state)  {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        }
+    }
+
+
+}
+
+	
+#endif
