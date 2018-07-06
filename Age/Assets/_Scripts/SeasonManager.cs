81bd5b334bebceb0f96a87802330c438589f280b
Geurtsy <36181392+Geurtsy@users.noreply.github.com>
2018-07-06 13:33:40 +0800

    Fade in and out effect on season change.

--- Age/Assets/_Scripts/SeasonManager.cs
+++ Age/Assets/_Scripts/SeasonManager.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SeasonManager : MonoBehaviour {
     
+    
     public enum Seasons { SUMMER, WINTER, AUTUMN, SPRING }
     public static Seasons _currentSeason;
+
+    [Header("Seasons")]
     [SerializeField] private Seasons[] _seasonOrder;
     private int _currentSeasonIndex;
 
@@ -18,8 +23,14 @@ public class SeasonManager : MonoBehaviour {
     private Coroutine _autumn;
     private Coroutine _spring;
 
+    [Header("Transition")]
+    [SerializeField] private float _fadeOutDuration;
+    [SerializeField] private float _fadeInDuration;
+    [SerializeField] private Image _fadeMask;
+
     private void Start()
     {
+        DontDestroyOnLoad(gameObject);
         _currentSeason = _seasonOrder[0];
         Setup();
     }
@@ -62,16 +73,40 @@ public class SeasonManager : MonoBehaviour {
         }
     }
 
-    public void ChangeSeason()
+    public IEnumerator ChangeSeason()
     {
         _currentSeasonIndex = _currentSeasonIndex < _seasonOrder.Length - 1 ? _currentSeasonIndex + 1 : 0;
         _currentSeason = _seasonOrder[_currentSeasonIndex];
 
+        print("Change Season");
+
+        var step = 0.0f;
+        Color startColour = _fadeMask.color;
+
+        while(step < 1)
+        {
+            step += Time.deltaTime/_fadeOutDuration;
+            _fadeMask.color = Color.Lerp(startColour, new Color(startColour.r, startColour.g, startColour.b, 1), step);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(_currentSeasonIndex);
+
+        step = 0.0f;
+        startColour = _fadeMask.color;
+
+        while(step < 1)
+        {
+            step += Time.deltaTime / _fadeInDuration;
+            _fadeMask.color = Color.Lerp(startColour, new Color(startColour.r, startColour.g, startColour.b, 0), step);
+            yield return null;
+        }
 
         if(_loopSeasons)
             WipeSeasons();
 
         Setup();
+        yield return null;
     }
 
     private void WipeSeasons()
@@ -99,7 +134,7 @@ public class SeasonManager : MonoBehaviour {
             yield return null;
         }
 
-        ChangeSeason();
+        StartCoroutine(ChangeSeason());
 
         yield return null;
     }
@@ -119,8 +154,7 @@ public class SeasonManager : MonoBehaviour {
             yield return null;
         }
 
-        ChangeSeason();
-
+        StartCoroutine(ChangeSeason());
         yield return null;
     }
 
@@ -139,8 +173,7 @@ public class SeasonManager : MonoBehaviour {
             yield return null;
         }
 
-        ChangeSeason();
-
+        StartCoroutine(ChangeSeason());
         yield return null;
     }
 
@@ -159,8 +192,7 @@ public class SeasonManager : MonoBehaviour {
             yield return null;
         }
 
-        ChangeSeason();
-
+        StartCoroutine(ChangeSeason());
         yield return null;
     }
 }
