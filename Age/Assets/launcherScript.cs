4c0dca8592422132abc8cc1e1d7742cb1efefc5a
kcrash201 <10089172@student.sae.edu.au>
2018-07-06 12:29:13 +0800

    Rainbow

---
+++ Age/Assets/launcherScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class launcherScript : MonoBehaviour {
+
+    public GameObject rainbow;
+    public float shootForce = 0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject projectile = (GameObject)Instantiate(
+                rainbow, transform.position, transform.rotation);
+            projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * shootForce);
+        }
+	}
+}
