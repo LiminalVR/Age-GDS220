b87207143152a542ac9cdb56483fe26744ee5aac
kcrash201 <10089172@student.sae.edu.au>
2018-07-20 10:13:43 +0800

    Camp fire increase
    space to activate

---
+++ Age/Assets/_Scripts/Campfire.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campfire : MonoBehaviour 
+{
+	ParticleSystem myParticleSystem;
+	ParticleSystem.EmissionModule emissionModule;
+	ParticleSystem.ShapeModule shape;
+
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			// Get the system and the emission module.
+			myParticleSystem = GetComponent<ParticleSystem> ();
+			emissionModule = myParticleSystem.emission;
+			ParticleSystem.ShapeModule shape = myParticleSystem.shape;
+
+			GetValue ();
+			SetValue ();
+			shape.radius = 0.7f;
+	}
+}
+	void GetValue()
+	{
+		//print("The constant value is " + emissionModule.rate.constant);
+	}
+
+	void SetValue()
+	{
+		emissionModule.rate = 100.0f;
+	}
+}
