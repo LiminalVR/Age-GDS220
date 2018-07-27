3ef4b5df771c4d545ab2de8051419b86a348d972
Geurtsy <36181392+Geurtsy@users.noreply.github.com>
2018-07-27 17:07:57 +0800

    Player Controller Active

--- Age/Assets/_Scripts/PlayerController.cs
+++ Age/Assets/_Scripts/PlayerController.cs
@@ -7,10 +7,31 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private LayerMask _interactionLayers;
     [SerializeField] private string _buttonName;
     [SerializeField] private float _maxDis;
+    [SerializeField] private float _mouseSensitivity = 15.0f;
     private IElement _selectedElement;
+
+    private float _xRot;
+    private float _yRot;
+    private Quaternion _originalRot;
+    private LineRenderer _lineRenderer;
+
+    private void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.useWorldSpace = false;
+        _lineRenderer.positionCount = 2;
+        _originalRot = transform.localRotation;
+    }
 
     private void Update()
     {
+
+
+
+        _xRot += Input.GetAxis("Mouse X") * _mouseSensitivity;
+        _yRot += Input.GetAxis("Mouse Y") * _mouseSensitivity;
+        transform.localRotation = _originalRot * Quaternion.AngleAxis(_xRot, Vector3.up) * Quaternion.AngleAxis(_yRot, -Vector3.right);
+
        if(Input.GetButtonDown(_buttonName))
        {
             Ray ray = new Ray(transform.position, transform.forward);
@@ -24,6 +45,14 @@ public class PlayerController : MonoBehaviour {
 
         if(Input.GetButton(_buttonName))
         {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit raycastHit;
+
+            if(Physics.Raycast(ray, out raycastHit, _maxDis, _interactionLayers))
+            {
+                _lineRenderer.SetPosition(1, new Vector3(0, 0, raycastHit.distance));
+            }
+
             if(_selectedElement != null)
             {
                 _selectedElement.ConfirmationTime += Time.deltaTime;
