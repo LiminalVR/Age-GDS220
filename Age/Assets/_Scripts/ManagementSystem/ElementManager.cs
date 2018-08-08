6d12b2fa58fd7af959ac9dbe980e964e9c61d41b
Geurtsy <36181392+Geurtsy@user.noreply.github.com>
2018-08-08 22:41:56 +0800

    Resizing On Disable - Aesthetics.

--- Age/Assets/_Scripts/ManagementSystem/ElementManager.cs
+++ Age/Assets/_Scripts/ManagementSystem/ElementManager.cs
@@ -10,6 +10,15 @@ public class ElementManager : MonoBehaviour {
     [HideInInspector] public ElementType[] _currentElementOrder;
     private int _nextElementIndex = 0;
 
+    [Header("Sizes")]
+    [SerializeField] private Vector3 _normalSize;
+    [SerializeField] private Vector3 _shrunkenSize;
+    [SerializeField] private Vector3 _enlargedSize;
+    [SerializeField] private float _adjustmentDuration;
+    [SerializeField] private float _stagnentDuration;
+
+    private List<BaseElement> _elementList;
+
     /*
     struct Element {
         public BaseElement _element;
@@ -23,6 +32,15 @@ public class ElementManager : MonoBehaviour {
     private void Awake()
     {
         DelegatesAndEvents._onElementAcivated += ElementActivated;
+    }
+
+    private void Start()
+    {
+        _elementList = new List<BaseElement>();
+        _elementList.Add(_earthElement);
+        _elementList.Add(_waterElement);
+        _elementList.Add(_fireElement);
+        _elementList.Add(_airElement);
     }
 
     public void SpawnElement()
@@ -56,11 +74,64 @@ public class ElementManager : MonoBehaviour {
 
     public void ElementActivated()
     {
+        StartCoroutine(AdjustElements(_currentElementOrder[_nextElementIndex - 1], _selectedElement));
+
         if(_nextElementIndex < _currentElementOrder.Length)
         {
             SelectNextElement();
             SpawnElement();
         }
+    }
+
+    private IEnumerator AdjustElements(ElementType activatedType, BaseElement activatedElement)
+    {
+        // Shrinking.
+        if(activatedType != ElementType.EARTH)
+            StartCoroutine(ResizeElement(_earthElement, _shrunkenSize, _adjustmentDuration));
+
+        if(activatedType != ElementType.WATER)
+            StartCoroutine(ResizeElement(_waterElement, _shrunkenSize, _adjustmentDuration));
+
+        if(activatedType != ElementType.FIRE)
+            StartCoroutine(ResizeElement(_fireElement, _shrunkenSize, _adjustmentDuration));
+
+        if(activatedType != ElementType.AIR)
+            StartCoroutine(ResizeElement(_airElement, _shrunkenSize, _adjustmentDuration));
+
+        if(activatedType != ElementType.SEASON)
+            StartCoroutine(ResizeElement(_seasonElement, _shrunkenSize, _adjustmentDuration));
+
+        // Enlarging.
+        StartCoroutine(ResizeElement(activatedElement, _enlargedSize, _adjustmentDuration));
+
+        yield return new WaitForSeconds(_adjustmentDuration + _stagnentDuration);
+
+        // Returning.
+        foreach(BaseElement element in _elementList)
+        {
+            StartCoroutine(ResizeElement(element, _normalSize, _adjustmentDuration));
+        }
+
+        yield return null;
+    }
+
+    private IEnumerator ResizeElement(BaseElement targetElement, Vector3 targetSize, float duration)
+    {
+        Vector3 startSize = targetElement.gameObject.transform.localScale;
+        Vector3 stepSize = Vector3.zero;
+        float step = 0.0f;
+
+        while(step < 1.0f)
+        {
+            stepSize = Vector3.Lerp(startSize, targetSize, step);
+            step += Time.deltaTime / duration;
+
+            targetElement.gameObject.transform.localScale = stepSize;
+
+            yield return null;
+        }
+
+        yield return null;
     }
 
     public void ResetElementOrder(ElementType[] order)
