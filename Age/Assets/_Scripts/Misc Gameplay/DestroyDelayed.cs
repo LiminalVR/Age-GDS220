4396058d7b4ccf294a99ae6ceae650abe6353e23
Chris-Stoj <1010691@student.sae.edu.au>
2018-08-15 17:27:43 +0800

    Terrain audio tweaks

---
+++ Age/Assets/_Scripts/Misc Gameplay/DestroyDelayed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyDelayed : MonoBehaviour {
+
+	void Start ()
+    {
+        Invoke("DestroyMe", 9f);
+	}
+	
+	void DestroyMe()
+    {
+        Destroy(gameObject);
+    }
+}
